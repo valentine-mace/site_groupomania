764000a1eeb5c26ae32b72c00db2a8779eb1c2a0
valentine-mace <valentine.mace@hotmail.com>
2022-05-12 21:45:17 +0200

    réorganisation

---
+++ back-end/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser =  require("body-parser");
+const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+const app = express();
+
+//pour problème CORS
+app.use((req, res, next) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+next();
+});
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+//pour multer
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+//routes
+// app.use('/api/auth', userRoutes);
+// app.use('/api/sauces', saucesRoutes);
+
+module.exports = app;
