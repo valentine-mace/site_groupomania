c360d506af186ea3035e5980e5bd9fc1faa54425
valentine-mace <valentine.mace@hotmail.com>
2022-05-30 11:32:44 +0200

    front end

---
+++ front-end/src/pages/ConnexionPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ConnexionPage() {
+  return (
+    <div>
+      <h1>Connexion</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/pages/InscriptionPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InscriptionPage() {
+  return (
+    <div>
+      <h1>Inscription</h1>
+    </div>
+  );
+}
+
+export default App;
