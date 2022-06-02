56c7bf380cf4d977fa03af2e2cb40f0fd3a688f7
valentine-mace <valentine.mace@hotmail.com>
2022-06-02 12:01:48 +0200

    logo cliquable

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -6,13 +6,13 @@ import { useParams} from "react-router-dom"
 
 const Header = () => {
   const current_pathname = window.location.pathname;
-  const userId = useParams();
+  const userId = useParams().id;
   return (
     <div className="header">
-      <NavLink to= {"/home"}>
-      {/* <img src="https://www.cerballiance.fr/sites/www.cerballiance.fr/files/styles/main_image_desktop/public/media/image/2020-12/iStock-1212779887-test-antigenique-achetee.jpg?h=c2a00083&itok=ygbJZHJq" alt="Logo Groupomania"></img> */}
-      </NavLink>
+      <NavLink to= {"/home/" + userId} >
       <img src={logo} alt="Logo Groupomania"></img>
+      </NavLink>
+
       {/* <nav>
         <ul>
           <NavLink to={current_pathname + "/profil" + userId}><li>Mon profil</li></NavLink>
