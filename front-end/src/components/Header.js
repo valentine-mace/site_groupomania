2291db27937e54f3a3b3d7a0638be15f577f3182
valentine-mace <valentine.mace@hotmail.com>
2022-06-02 13:27:10 +0200

    header affiché en fonction

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -2,21 +2,29 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "./icon-left-font.png";
 import { useParams} from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
 
 const Header = () => {
   const current_pathname = window.location.pathname;
   const userId = useParams().id;
+  let location = useLocation().pathname;
+  console.log(location);
   return (
     <div className="header">
       <NavLink to= {"/home/" + userId} >
       <img src={logo} alt="Logo Groupomania"></img>
       </NavLink>
-      <nav>
-        <ul>
-          <NavLink to={current_pathname + "/profil/" + userId}><li>Mon profil</li></NavLink>
-        </ul>
-      </nav>
+      {location == "/home/" + userId &&
+        <nav>
+          <ul>
+            <NavLink to={current_pathname + "/profil/" + userId}><li>Mon profil</li></NavLink>
+          </ul>
+          <ul>
+            <NavLink to={"/"}><li>Se déconnecter</li></NavLink>
+          </ul>
+        </nav>  
+      }
     </div>
   );
 }
